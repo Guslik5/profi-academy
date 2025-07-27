1045dd0f0349013ec5a5560a2f99a9cdc0e66f6e
Guslik5 <guslik2003@mail.ru>
2025-07-28 01:47:48 +0300

    added header, without mobile version

---
+++ src/components/Header.jsx
@@ -0,0 +1,109 @@
+import React from 'react';
+import Button from 'react-bootstrap/Button';
+import {Container, Nav, Navbar, NavDropdown} from "react-bootstrap"; // Импортируем компонент Button
+import logo from '../assets/logo.png'
+import searchIcon from '../assets/searchIcon.png'
+import styled from 'styled-components';
+
+const StyledNavDropdown = styled(NavDropdown)`
+  &.nav-link {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: 1.25rem;
+  }
+    
+  .dropdown-menu {
+    border-radius: 15px;
+    box-shadow: 5px 5px 10px rgba(0, 0, 0, 0.2);
+    border: none;
+    padding: 10px;
+  }
+    
+  .dropdown-menu .dropdown-item {
+    border-radius: 10px;
+      &:active {
+          background-color: #00776B  !important;; /* Более темный оттенок при клике */
+          border-color: #00776B  !important;;
+          box-shadow: none  !important;; /* Убираем тень при клике */
+      }
+  }
+`;
+const StyledButton = styled(Button)`
+    height: 70px;
+    background-color: #00998B;
+    border: 1px solid #00998B;
+    border-radius: 20px;
+    color: white;
+
+    &:hover {
+        box-shadow: none;
+        background-color: #00776B;
+        border-color: #00776B;
+    }
+
+    /* Смена цвета при клике */
+    &:active {
+        background-color: #00554B  !important;; /* Более темный оттенок при клике */
+        border-color: #00554B  !important;;
+        box-shadow: none  !important;; /* Убираем тень при клике */
+    }
+`;
+
+
+
+function Header() {
+    return (
+        <Navbar expand="lg" className="bg-body-tertiary mx-5 " style={{borderBottom: "1px solid #9E9E9E"}}>
+                <Navbar.Brand href="#home">
+                    <img
+                        src={logo}
+                        alt="Логотип"
+                        height="94"
+                        className="d-inline-block align-top mx-5"
+                    />
+                </Navbar.Brand>
+
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="d-flex justify-content-between w-100">
+                        <Nav.Item className="d-flex justify-content-center align-items-center fs-5 ">
+                            <img src={searchIcon} alt="Лупа"/>
+                            поиск
+                        </Nav.Item>
+                        <StyledNavDropdown title="О нас" id="about" className="d-flex justify-content-center align-items-center fs-5">
+                            <NavDropdown.Item href="#action/3.1">Наша миссия</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.2">Документы</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.3">Контакты</NavDropdown.Item>
+                        </StyledNavDropdown>
+
+                        <StyledNavDropdown title="Обучение" id="study" className="d-flex justify-content-center align-items-center fs-5 ">
+                            <NavDropdown.Item href="#action/3.1">Рабочие профессии</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.2">Повышение квалификации</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.3">Профессиональная переподготовка</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.3">Пожарная безопасность</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.3">Охрана труда, первая помощь, высота, ОЗП</NavDropdown.Item>
+                        </StyledNavDropdown>
+
+                        <StyledNavDropdown title="Консалтинг" id="consulting" className="d-flex justify-content-center align-items-center fs-5 ">
+                            <NavDropdown.Item href="#action/3.1">Лицензирование</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.2">Аттестация</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.3">Вступление в СРО</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.3">Пожарный аудит</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.3">СОУТ</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.3">Расчет рисков</NavDropdown.Item>
+                        </StyledNavDropdown>
+
+                        <div className="d-flex flex-column justify-content-center align-items-center" style={{minWidth: "171px"}}>
+                            <Nav.Link href="tel:+71234567890" className="p-0">+7 (123) 456-78-90</Nav.Link>
+                            <Nav.Link href="mailto:akademia-profi@mail.ru" className="p-0">akademia-profi@mail.ru</Nav.Link>
+                        </div>
+
+                        <StyledButton className="px-3">Оставить заявку</StyledButton>
+                    </Nav>
+                </Navbar.Collapse>
+        </Navbar>
+    );
+}
+
+export default Header;
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,12 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React from 'react';
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import '@fontsource/ibm-plex-serif/400.css'; // Regular
+import '@fontsource/ibm-plex-serif/700.css'; // Bold
+import '@fontsource/ibm-plex-serif/400-italic.css'; // Italic
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
